148a293264d665ab12708838c009668f099942de
ezequielmapel <ezequiel.f.mapel@gmail.com>
2022-06-17 19:25:59 -0300

    template basico da homepage

--- ticketbrabo-frontend/src/app/pages/home-page/home-page.module.ts
+++ ticketbrabo-frontend/src/app/pages/home-page/home-page.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { HomePageRoutingModule } from "./home-page-routing.module";
 import { HomePageComponent } from "./home-page.component";
-
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
     declarations: [HomePageComponent],
     imports: [
         CommonModule, 
         RouterModule,
-        HomePageRoutingModule
+        HomePageRoutingModule,
+        MatCardModule
     ],
 })
 export class HomePageModule { }
