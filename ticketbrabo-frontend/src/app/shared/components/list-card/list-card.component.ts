fe7c96bd8d3b56ddeff5d3703eece397c26b020b
ezequielmapel <ezequiel.f.mapel@gmail.com>
2022-06-27 18:35:53 -0300

    alteracao list card

--- ticketbrabo-frontend/src/app/shared/components/list-card/list-card.component.ts
+++ ticketbrabo-frontend/src/app/shared/components/list-card/list-card.component.ts
@@ -1,21 +1,28 @@
-import { Component, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-list-card',
   templateUrl: './list-card.component.html',
   styleUrls: ['./list-card.component.css']
 })
-export class ListCardComponent implements OnInit {
+export class ListCardComponent implements OnInit, OnDestroy {
 
   @Input()
   public listCard!: any[];
   private activeCardItem: any;
 
+  private changeCard = new EventEmitter();
+  private subscription!: Subscription;
+
   constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
-   
+   this.subscription = this.changeCard.subscribe((c) => this.onSelectedCard(c));
+  }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   onClickCard(cardId: string, event: any): void {
@@ -42,4 +49,24 @@ export class ListCardComponent implements OnInit {
     return target.path.filter((p: any) => p.classList && p.classList.contains("cardItem"))[0];
   }
 
+  toggleClass(newSelectedCard: any) {
+    if (this.activeCardItem && this.activeCardItem != newSelectedCard) {
+      this.toggleClassAndActiveCard(this.activeCardItem, { key: "border", value: "2px solid transparent" });
+      this.toggleClassAndActiveCard(newSelectedCard, { key: "border", value: "2px solid var(--hoverColor)" });
+      return;
+    }
+
+    this.toggleClassAndActiveCard(newSelectedCard, { key: "border", value: "2px solid var(--hoverColor)" });
+  }
+
+  emitCardChange(event: any) {
+    const card = this.isClickOnCard(event);
+    if(card){
+      this.changeCard.emit(card);
+    }
+  }
+
+  onSelectedCard(card: any) {
+    this.toggleClass(card);
+  }
 }
